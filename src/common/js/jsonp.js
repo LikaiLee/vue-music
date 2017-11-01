d15b122b6ffccb958ed58522585bac2bfdf41d9d
LikaiLee <likailee.cn@gmail.com>
2017-11-01 19:47:37 +0800

    推荐 -> 轮播图完成

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.includes('?') ? '&' : '?') + addParams(data)
+
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(data)
+      }
+    })
+  })
+}
+
+function addParams(data) {
+  let url = ''
+
+  for (let k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+
+  return url ? url.substring(1) : ''
+}
